c289c4acaa69ec5bdaf46f03bdf82940a9874e8d
Nana Hauwa Abdulsalam <68066734+Amal-Salam@users.noreply.github.com>
2023-03-27 15:41:27 +0100

    Create User.js

---
+++ api/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema({
+  firstname:{type: String, required: true}, 
+  surname:{type: String,required:true}, 
+  username: { type: String, required: true, min: 4, unique: true },
+  password: { type: String, required: true },
+});
+
+const UserModel = model('User', UserSchema);
+
+module.exports = UserModel;
